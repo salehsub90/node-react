0cd99ef0e3909cad754187bd6a09156c6226ae2f
Saleh Suboh <subohsaleh@gmail.com>
2026-08-28 09:36:47 -0400

    use client build files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
+const path = require('path');
 const keys = require('./config/keys');
 const passport = require('passport');
 require('./models/User');
@@ -20,6 +21,11 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    app.use(express.static('client/build'));
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
