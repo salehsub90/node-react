c1b30241cc71589a87a6bd8f1a0fe454d76f35e7
Saleh Suboh <subohsaleh@gmail.com>
2026-08-31 10:42:39 -0400

    Quick changes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
-const path = require('path');
 const keys = require('./config/keys');
 const passport = require('passport');
 const bodyParser = require('body-parser');
@@ -29,7 +28,8 @@ if (process.env.NODE_ENV === 'production') {
     // Express will serve up production assets
     app.use(express.static('client/build'));
     // Express will serve up the index.html file if it doesn't recognize the route
-    app.use((req, res) => {
+    const path = require('path');
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
